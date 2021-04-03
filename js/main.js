67f504c52f5071a10eaed9752d19094fa2837b20
KleimyonovVladimir <kleimionovvova@gmail.com>
2021-04-03 11:20:26 +0300

    Combo section is ready

--- js/main.js
+++ js/main.js
@@ -20,4 +20,12 @@ $(document).ready(() => {
       },
     },
   });
+  new Swiper(".combo-swiper-container", {
+    slidesPerView: 3.3,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 });
